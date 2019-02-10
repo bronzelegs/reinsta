3e54e295b13edecb231734b810813ec3d0c85caf
Terry Davis <tdavis0525@gmail.com>
2019-02-10 09:48:12 -0800

    initial checkin
    initial checkin
    
    simple stub

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
+import Post from './components/Post';
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <div className="App">
+        <Header />
+        <section className="App-main">
+          <Post nickname="Selena" avatar="https://openclipart.org/download/247324/abstract-user-flat-1.svg" caption="Moving the community!" image="https://i2-prod.mirror.co.uk/incoming/article13215795.ece/ALTERNATES/s810/0_MAIN-JFP_CHP_090918selenagomez_9339-JPG.jpg" />
+          <Post nickname="Selena" avatar="https://openclipart.org/download/247324/abstract-user-flat-1.svg" caption="Holding a mic" image="https://i2-prod.mirror.co.uk/incoming/article13215795.ece/ALTERNATES/s810/0_MAIN-JFP_CHP_090918selenagomez_9339-JPG.jpg" />
+    {/*
+
+          <img alt="Icon Living" src="https://i2-prod.mirror.co.uk/incoming/article13215795.ece/ALTERNATES/s810/0_MAIN-JFP_CHP_090918selenagomez_9339-JPG.jpg" />
+          <img src="https://openclipart.org/download/247324/abstract-user-flat-1.svg" alt="Chris" />
+
+       more posts */}
+        </section>
+      </div>;
   }
 }
 
